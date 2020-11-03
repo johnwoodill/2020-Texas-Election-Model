75bf41ca9a789018b7437804745359de2fe701a6
sweenejo <sweenejo@gmail.com>
2020-11-02 19:20:57 -1000

    updated observed votes, added two histogram figs

--- Figures.R
+++ Figures.R
@@ -1,17 +1,36 @@
 library(tidyverse)
 library(scales)
 
-setwd("~/Projects/2020-Texas-Election-Model/")
+#setwd("~/Projects/2020-Texas-Election-Model/")
 
 #Jon's working directory
-#setwd("/Users/Jon/Dropbox/Documents/consulting_projects/2020-Texas-Election-Model/")
+setwd("/Users/Jon/Dropbox/Documents/consulting_projects/2020-Texas-Election-Model/")
 
 dat <- read_csv("data/processed_election_results.csv")
 
 
 head(dat)
 
-pdat <- dat %>% 
+# Calculate observed votes from counties reporting
+obs_dat <- dat %>%
+  filter(corr_county == county) %>%
+  group_by(county, N) %>%
+  summarize(obs_v = first(pred_v))
+
+# Replace predicted with observed for counties observed
+obs_pdat <- dat %>%
+  left_join(obs_dat, by = c("county", "N")) %>%
+  mutate(pred_v = ifelse(is.na(obs_v) == F, obs_v, pred_v)) %>%
+  group_by(county, pred_year, N) %>% 
+  summarise(mean_pred_v = mean(pred_v),
+            sd_pred_v = sd(pred_v)) %>% 
+  ungroup() %>% 
+  group_by(pred_year, N) %>% 
+  summarise(total_v = sum(mean_pred_v)) %>% 
+  group_by(pred_year) %>% 
+  mutate(perc = (total_v / sum(total_v)) * 100)
+
+pdat <- dat %>%
   group_by(county, pred_year, N) %>% 
   summarise(mean_pred_v = mean(pred_v),
             sd_pred_v = sd(pred_v)) %>% 
@@ -26,6 +45,10 @@ per_report <- round((n_distinct(dat$corr_county) / n_distinct(dat$county)) * 100
 
 pdat$N <- factor(pdat$N, levels = c("TRUMP", "BIDEN", "OTHER"))
 
+#########
+# Remember to uncomment/comment the line below the data starts flowing.
+#########
+#ggplot(obs_pdat, aes(x=factor(pred_year),  y=perc, fill=factor(N))) + 
 ggplot(pdat, aes(x=factor(pred_year),  y=perc, fill=factor(N))) + 
   geom_bar(stat='identity', position = "dodge") +
   geom_text(aes(label=round(perc, 2)), position=position_dodge(width=0.75), vjust=-0.5, size=2.5) +
@@ -35,7 +58,7 @@ ggplot(pdat, aes(x=factor(pred_year),  y=perc, fill=factor(N))) +
   theme(legend.title = element_blank(),
         legend.position = c(0.9, 0.85)) +
   ylim(0, 100) +
-  # annotate("text", x = as.factor(2012), y = 75, label = str_c(as.character(per_report), "% reporting")) +
+  annotate("text", x = as.factor(2014), y = 100, label = str_c(as.character(per_report), str_c("% reporting as of ", as.character(format(Sys.time(), format="%H:%M %Z"))))) +
   NULL
 
 ggsave("figures/predictions.png", width=6, height=4)  
@@ -90,6 +113,50 @@ ggplot(pdat1, aes(y=w_perc, x=election, fill=N)) +
 
 ggsave("figures/bar_predictions.png", width=6, height=2)  
 
-  
 
+# Representative turnout histogram
+turnout_pdat <- dat %>%
+  left_join(obs_dat, by = c("county", "N")) %>%
+  mutate(pred_v = ifelse(is.na(obs_v) == F, obs_v, pred_v)) %>%
+  group_by(corr_county, N, pred_year) %>%
+  summarise(total_v = sum(pred_v)) %>% 
+  spread(N, total_v) %>%
+  mutate(sum_total = BIDEN + TRUMP + OTHER) %>%
+  mutate(BIDEN_per = round(BIDEN / sum_total * 100, 1),
+         TRUMP_per = round(TRUMP / sum_total * 100, 1)) %>%
+  mutate(BIDEN_points = BIDEN_per - TRUMP_per) %>%
+  mutate(color = ifelse(BIDEN_points > 0, "B_plus", "B_neg"))
+
+col_pal = c("B_plus" = "darkblue",
+           "B_neg" = "red")
+
+ggplot(turnout_pdat) +
+  geom_histogram(aes(x = BIDEN_points, fill = color),  binwidth = 0.1) +
+  scale_x_continuous(breaks = seq(-10, 10, by = 0.1)) +
+  theme_classic() +
+  theme(legend.position = "none") +
+  scale_fill_manual(
+    values = col_pal,
+    limits = names(col_pal)
+  ) +
+  facet_grid(rows = vars(pred_year)) +
+  labs(title = "Uncertainty in county turnout", x = "Percentage points for Biden", y = "Number of predictions")
+
+
+ggsave("figures/county_turnout_histo.png", width=6, height=4)  
+
+# Combined county correlation turnout uncertainty histogram
+ggplot(turnout_pdat) +
+  geom_histogram(aes(x = BIDEN_points, fill = color),  binwidth = 0.1) +
+  scale_x_continuous(breaks = seq(-10, 10, by = 0.1)) +
+  theme_classic() +
+  theme(legend.position = "none") +
+  scale_fill_manual(
+    values = col_pal,
+    limits = names(col_pal)
+  ) +
+  labs(title = "Combining unknown county turnout and correlations", x = "Percentage points for Biden", y = "Number of predictions")
+
+
+ggsave("figures/county_corr_turnout_histo.png", width=6, height=4)  
 
